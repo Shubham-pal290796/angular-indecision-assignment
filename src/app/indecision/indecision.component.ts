6fdf6ca6d4ff13f2b332f94e46ae5dae43cfdce6
Shubham-pal290796 <shubham.pal@infosys.com>
2020-03-19 09:39:37 +0530

    2 files modified, 1 added

--- src/app/indecision/indecision.component.ts
+++ src/app/indecision/indecision.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndecisionComponent implements OnInit {
 
+  inputArray = [];
+  showInitialText : boolean = true;
+  inputVal : string = '';
+  input : string = "";
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  handleInput(event) {
+    this.inputVal = event.target.value;
+  }
+
+  submit() {
+    if (this.inputVal != "") {
+      this.inputArray.push(this.inputVal);
+      if (this.inputArray.length > 0) {
+        this.showInitialText = false;
+      }
+    }
+    this.input = null;
+  }
+
+  removeAll() {
+    this.inputArray = [];
+    this.showInitialText = true;
+  }
+
 
 }
